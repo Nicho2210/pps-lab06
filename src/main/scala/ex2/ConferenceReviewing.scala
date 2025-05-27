04ac2482142ea490c66b6906261aeec412431141
RicciNicholas <riccinicho22@gmail.com>
2025-05-27 20:43:58 +0200

    implemented loadReview and orderedScores

--- src/main/scala/ex2/ConferenceReviewing.scala
+++ src/main/scala/ex2/ConferenceReviewing.scala
@@ -2,7 +2,7 @@ package ex2
 
 enum Question:
   case Relevance, Significance, Confidence, Final
-  
+
 trait ConferenceReviewing:
   def loadReview(article: Int, scores: Map[Question, Int]): Unit
   def loadReview(article: Int, relevance: Int, significance: Int, confidence: Int, fin: Int): Unit
@@ -11,28 +11,35 @@ trait ConferenceReviewing:
   def acceptedArticles(): Set[Int]
   def sortedAcceptedArticles(): List[(Int, Double)]
   def averageWeightedFinalScoreMap(): Map[Int, Double]
-  
+
 object ConferenceReviewing:
   def apply(): ConferenceReviewing = ConferenceReviewingImpl()
-  
-  private class ConferenceReviewingImpl() extends ConferenceReviewing:
-    override def loadReview(article: Int,
-                            scores: Map[Question, Int]): Unit = ???
 
-    override def loadReview(article: Int,
-                            relevance: Int,
-                            significance: Int,
-                            confidence: Int,
-                            fin: Int): Unit = ???
+  private case class ConferenceReviewingImpl() extends ConferenceReviewing:
 
-    override def orderedScores(article: Int,
-                               question: Question): List[Int] = ???
+    private var _reviews: List[(Int, Map[Question, Int])] = List.empty
+    
+    override def loadReview(article: Int, scores: Map[Question, Int]): Unit =
+      _reviews = (article, scores) :: _reviews
+    
+    import Question.*
+    override def loadReview(article: Int, 
+                            relevance: Int, 
+                            significance: Int, 
+                            confidence: Int, 
+                            fin: Int): Unit =
+      _reviews = (article, Map(Relevance -> relevance, 
+        Significance -> significance, 
+        Confidence -> confidence, 
+        Final -> fin)) :: _reviews
+
+    override def orderedScores(article: Int, question: Question): List[Int] =
+      _reviews.filter((a, _) => a == article).map((_, m) => m).map(m => m(question)).sorted
 
     override def averageFinalScore(article: Int): Double = ???
 
-    override def acceptedArticles(): Set[Int] = ???
+    override def acceptedArticles(): Predef.Set[Int] = ???
 
     override def sortedAcceptedArticles(): List[(Int, Double)] = ???
 
-    override def averageWeightedFinalScoreMap(): Map[Int, Double] = ???
-    
+    override def averageWeightedFinalScoreMap(): Predef.Map[Int, Double] = ???
